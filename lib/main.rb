2910b9dbd91329394484363c01d83f3ae06a6b35
EphraimKariuki <ephraimkabuikariuki@gmail.com>
2023-12-21 13:41:42 +0300

    Added a check_game_over method

--- lib/main.rb
+++ lib/main.rb
@@ -12,7 +12,9 @@ class Game
     def initialize(word)
         @word = word
         @guess = []
-        @guessed_word = Array.new(@word.length)
+        @guessed_word = Array.new(@word.length, "_")
+        @max_guesses = @word.length * 3
+        game_over = false
     end
     def play
         puts "The word is #{@word.length} letters long"
@@ -20,18 +22,41 @@ class Game
         letter = gets.chomp.to_s
         @guess.push(letter)
         check_letter_in_word(letter)
+        
     end
     def check_letter_in_word(letter)
-        if @word.include?(letter)
-            @word.split("").each_with_index do |item, index|
-                if item == letter
-                    @guessed_word[index] = item
-                else 
-                    @guessed_word[index] = "_"
-                end
-            end    
+        check_game_over
+        
+        if @game_over
+            puts "The correct word is : #{@word}"
+        else 
+            if @word.include?(letter)
+                @word.split("").each_with_index do |item, index|
+                    if item == letter
+                        @guessed_word[index] = item
+                    end
+                end 
+                puts "Your guess is correct. Hint: #{@guessed_word.join("")}"  
+            else 
+                puts "Your guess is wrong. Hint: #{@guessed_word.join("")}"
+            end
+            play 
+        end      
+    end
+
+    def check_game_over
+        if @guess.length == @max_guesses
+            
+            puts "You've reached your maximum number of guesses"
+            puts "Game over"
+            @game_over = true
+        elsif @guessed_word.join("") == @word
+            puts "You guessed the correct word"
+            puts "Game over"
+            @game_over = true
+        else
+            @game_over = false
         end
-        puts @guessed_word.join("")
     end
 end 
 
